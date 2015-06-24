922da3512b5083bf6d2a149796c03fa8b6e0b964
Kenzo Lobos <knzo@live.cl>
2015-06-24 12:54:01 -0300

    Now Displays numbers correctly.

--- PongVGA2/NumberGenerator.v
+++ PongVGA2/NumberGenerator.v
@@ -36,25 +36,27 @@ module NumberGenerator
 	parameter EIGHT = 4'b1000;
 	parameter NINE  = 4'b1001;
 	
+	// This are ordered backwards for correct display.
 	reg [2:0] a = 3'b000;
-	reg [2:0] b = 3'b001;
+	reg [2:0] b = 3'b100;
 	reg [2:0] c = 3'b010;
-	reg [2:0] d = 3'b011;
-	reg [2:0] e = 3'b100;
+	reg [2:0] d = 3'b110;
+	reg [2:0] e = 3'b001;
 	reg [2:0] f = 3'b101;
-	reg [2:0] g = 3'b110;
+	reg [2:0] g = 3'b011;
 	reg [2:0] h = 3'b111;
 	
+	// This are ordered backwards for correct display.
 	wire [14:0] zero  = {h,f,f,f,h} ;
-	wire [14:0] one   = {c,g,c,c,c};
-	wire [14:0] two   = {h,f,d,e,h};
-	wire [14:0] three = {h,b,h,b,h};
-	wire [14:0] four  = {f,f,h,b,b};
-	wire [14:0] five  = {h,e,h,b,h};
-	wire [14:0] six   = {h,e,h,f,h};
-	wire [14:0] seven = {h,b,c,c,e};
-	wire [14:0] eight = {h,f,h,f,h};
-	wire [14:0] nine  = {h,f,h,b,h};	
+	wire [14:0] one   = {c,c,c,g,c};
+	wire [14:0] two   = {h,e,d,f,h}; //{h,f,d,e,h}
+	wire [14:0] three = {h,b,h,b,h}; //{h,b,h,b,h}
+	wire [14:0] four  = {b,b,h,f,f}; //{f,f,h,b,b}
+	wire [14:0] five  = {h,b,h,e,h}; //{h,e,h,b,h}
+	wire [14:0] six   = {h,f,h,e,h}; //{h,e,h,f,h}
+	wire [14:0] seven = {e,c,c,b,h}; //{h,b,c,c,e}
+	wire [14:0] eight = {h,f,h,f,h}; //{h,f,h,f,h}
+	wire [14:0] nine  = {h,b,h,f,h}; //{h,f,h,b,h}	
 	
 	always@(number, position)
 	begin
